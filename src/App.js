3a83a4e8cf0f7e8d9958e70db7898a393cc9dd15
mker <krmichael25@gmail.com>
2024-11-18 20:33:27 +0500

    Add React, rebuilding the app structure

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import "./style.css";
+
+function App() {
+  const appTitle = "Today I Learned";
+
+  return (
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" alt="Today I Learned Logo" />
+          <h1>{appTitle}</h1>
+        </div>
+
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+
+      <NewFactForm />
+
+      <main className="main">
+        <CategoryFilter />
+
+        <FactList />
+      </main>
+    </>
+  );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category Filter</aside>;
+}
+
+function FactList() {
+  return <section>Facts list</section>;
+}
+
+export default App;
